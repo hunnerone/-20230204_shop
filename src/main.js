c3a82b3d5425b5ca9d4ad6b1f45024e67bc9e814
zzc <2317070722@qq.com>
2023-02-04 00:38:27 +0800

    init -app

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// vue浏览控制台的非生产环境打包提示
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: (h) => h(App)
 }).$mount('#app')
+
+// 关闭局部的eslint 提示错误
+/* eslint-disable no-unused-vars*/
+// const a = 11
